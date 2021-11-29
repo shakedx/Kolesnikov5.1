9fa79256fbaff92e079641b9c677f090debd858e
shakedx <kolesnikov_danil123@mail.ru>
2021-11-29 16:24:37 +0200

    Добавьте файлы проекта.

---
+++ Kolesnikov5.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Kolesnikov5._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите строку: ");
+            string a = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите символ для нахождения: ");
+            string b = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите второй символ для нахождения: ");
+            string c = Convert.ToString(Console.ReadLine());
+            int str1 = 0, index = 0, str2 = 0;
+            while ((index = a.IndexOf(b, index) + 1) != 0) str1++;
+            while ((index = a.IndexOf(c, index) + 1) != 0) str2++;
+            Console.WriteLine("Символ " + b + " встречается в тексте -> " + str1 + " раз(а)");
+            Console.WriteLine("Символ " + c + " встречается в тексте -> " + str2 + " (а)");
+            Console.ReadKey();
+        }
+    }
+}
